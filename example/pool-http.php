018d8c1eebce54c5c44e8240aed9b6c8386f6c6a
azjezz <azjezz@protonmail.com>
2021-11-30 18:11:32 +0100

    -
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- example/pool-http.php
+++ example/pool-http.php
@@ -7,21 +7,21 @@ require __DIR__ . '/../vendor/autoload.php';
 $pool = new Really\Pool(__DIR__ . '/worker-http.php', 5, 2);
 $time = time();
 $awaitables = [];
-for ($i = 0; $i <= 10; $i++) {
-    $awaitables[] = $pool->dispatch(static function (Network\SocketInterface $connection): void {
+for ($i = 0; $i < 10; $i++) {
+    $awaitables[] = $pool->dispatch(static function (Network\SocketInterface $connection) use($i): void {
         $connection->writeAll('olleh');
         $response = $connection->readAll();
 
         ['worker' => $worker, 'response' => $response] = Json\decode($response);
 
-        echo "[worker=$worker]: $response\n";
+        echo "[worker=$worker][job=$i]: $response\n";
 
         $connection->close();
     });
 }
 
-$pool->stop();
-
 Async\all($awaitables);
+
+$pool->stop();
 
 var_dump(time() - $time); // executed 800 jobs.
